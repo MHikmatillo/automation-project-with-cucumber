ef2c7cd3f4158b6be2ba3769e42699ea28a391d1
khikmatillo <khikmatillo@khikmatillosmbp.lan>
2017-04-15 18:27:13 -0400

    Calculator

--- src/test/java/com/cbt/DemoTest.java
+++ src/test/java/com/cbt/DemoTest.java
@@ -1,5 +1,7 @@
 package com.cbt;
 
+import java.util.Scanner;
+
 public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
@@ -9,6 +11,17 @@ public class DemoTest {
 		for(int num=0 ; num<=100; num++){
 			if(num%2==0){
 				System.out.println(num);
+				
+				Scanner scan = new Scanner(System.in);  
+			     
+				  System.out.print("Input a number: ");  
+				  int num1 = scan.nextInt();  
+				     
+				  for (int i=0; i< 10; i++){  
+				   System.out.println(num1 + " x " + (i+1) + " = " +   
+				     (num1 * (i+1)));  
+				  }  
+				
 			}
 		}
 		
